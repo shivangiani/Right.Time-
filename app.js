8eb784979862b4799f390d1c806a63bee7cb3e53
Shivangi Singh <86972603+shivangiani@users.noreply.github.com>
2023-03-19 21:37:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var form = document.getElementById('form');
+
+form.addEventListener('submit',function(event){
+    event.preventDefault()
+
+    var email =document.getElementById('email').value;
+})
+
